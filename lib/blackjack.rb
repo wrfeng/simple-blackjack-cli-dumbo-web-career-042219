7ee958e736fa19e192da53fff8e922e1c2918231
wei feng <theweifeng@gmail.com>
2019-04-11 19:34:50 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -23,8 +23,9 @@ def get_user_input
   gets.chomp
 end
 
-def end_game
+def end_game(card_total)
   # code #end_game here
+  "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
