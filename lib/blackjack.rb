c00f9df386e9e12b94000bd31c15bd3a2f2537bb
wei feng <theweifeng@gmail.com>
2019-04-11 19:46:28 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,46 +1,43 @@
 def welcome
-  # code #welcome here
   puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
-  # code #deal_card here
   rand(1..11)
 end
 
 def display_card_total(card_total)
-  # code #display_card_total here
   puts "Your cards add up to #{card_total}"
 end
 
 def prompt_user
-  # code #prompt_user here
   puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  # code #get_user_input here
   gets.chomp
 end
 
 def end_game(card_total)
-  # code #end_game here
   puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
-  # code #initial_round here
   sum = deal_card + deal_card
   display_card_total(sum)
   sum
 end
 
-def hit?
-  # code hit? here
+def hit?(card_total)
+  prompt_user
+  input = get_user_input
+  
+  case input 
+  
+  when 
 end
 
 def invalid_command
-  # code invalid_command here
 end
 
 #####################################################
